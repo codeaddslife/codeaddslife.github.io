2fd2f1cd0bfab2144940649941d6cc342f091907
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2018-02-26 12:44:26 +0100

    detect broken links

--- site.js
+++ site.js
@@ -7,18 +7,34 @@ const matter = require('gray-matter');
 const mkdirp = require('mkdirp');
 const path = require('path');
 var highlight = require('pygments').colorizeSync;
-var matterOptions = { parser: require('js-yaml').safeLoad }
+var matterOptions = {parser: require('js-yaml').safeLoad}
 var layout = fs.readFileSync('layout.hbs').toString();
 var template = Handlebars.compile(layout);
 var minify = require('html-minifier').minify;
+var urlExists = require('url-exists');
 
 globby.sync('**/*.kd').forEach(function (kd) {
     var fileMatter = matter.read(kd, matterOptions);
-    var title = fileMatter.data.title ? fileMatter.data.title + " - codeaddslife" :  "codeaddslife";
+    var title = fileMatter.data.title ? fileMatter.data.title + " - codeaddslife" : "codeaddslife";
 
     var result = new koara.Parser().parse(fileMatter.content)
     var renderer = new koaraHtml.Html5Renderer();
     renderer.headingIds = true;
+
+    renderer.visitLink = function (node) {
+        urlExists('https://www.google.com', function(err, exists) {
+            console.log("Check " + node.value + "... " + (exists ? "OK!" : "NOK!"));
+            if(!exists) {
+                process.exit(1);
+            }
+        });
+
+        this.out += "<a href=\"" + this.escapeUrl(node.value.toString()) + "\">";
+        node.childrenAccept(this);
+        this.out += "</a>";
+    }
+
+
     result.accept(renderer);
 
     var html = template({"title": title, "body": renderer.getOutput()});
