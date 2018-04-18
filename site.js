75044f344ab536de7d9bb50d5d0ab11a1c76a1ae
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2018-04-18 11:01:04 +0200

    sort feed by update date

--- site.js
+++ site.js
@@ -6,34 +6,36 @@ const koaraHtml = require('koara-html');
 const matter = require('gray-matter');
 const mkdirp = require('mkdirp');
 const path = require('path');
-var highlight = require('pygments').colorizeSync;
-var matterOptions = {parser: require('js-yaml').safeLoad}
-var layout = fs.readFileSync('layout.hbs').toString();
-var template = Handlebars.compile(layout);
-var minify = require('html-minifier').minify;
-var urlExists = require('url-exists');
-var feedItems = [];
+const matterOptions = {parser: require('js-yaml').safeLoad}
+const layout = fs.readFileSync('templates/layout.hbs').toString();
+const template = Handlebars.compile(layout);
+const minify = require('html-minifier').minify;
+const feedItems = [];
 
 globby.sync('**/*.kd').forEach(function (kd) {
-    var fileMatter = matter.read(kd, matterOptions);
-    var title = fileMatter.data.title ? fileMatter.data.title + " - codeaddslife" : "codeaddslife";
+    const fileMatter = matter.read(kd, matterOptions);
+    const title = fileMatter.data.title ? fileMatter.data.title + " - codeaddslife" : "codeaddslife";
 
-    var result = new koara.Parser().parse(fileMatter.content)
-    var renderer = new koaraHtml.Html5Renderer();
+    const result = new koara.Parser().parse(fileMatter.content)
+    const renderer = new koaraHtml.Html5Renderer();
     renderer.headingIds = true;
     result.accept(renderer);
 
-    var html = template({"title": title, "body": renderer.getOutput()});
-    var htmlMin = minify(html, {collapseWhitespace: true, removeComments: true});
+    const html = template({"title": title, "body": renderer.getOutput()});
+    const htmlMin = minify(html, {collapseWhitespace: true, removeComments: true});
     mkdirp.sync(path.dirname(kd));
 
-    var fileName = kd.toString().substring(0, kd.toString().length - 3) + ".html";
+    const fileName = kd.toString().substring(0, kd.toString().length - 3) + ".html";
     fs.writeFileSync(fileName, htmlMin);
     if(path.dirname(kd) === 'articles') {
-        feedItems.push({title: fileMatter.data.title, link: 'https://www.codeaddslife.com/' + fileName, description: renderer.getOutput()})
+        feedItems.push({title: fileMatter.data.title, link: 'https://www.codeaddslife.com/' + fileName, description: renderer.getOutput(), pubDate: fileMatter.data.dateUpdated ?  fileMatter.data.dateUpdated : fileMatter.data.datePublished})
     }
 });
 
+
+
+
 // create rssFeeds
-var feedTemplate =  Handlebars.compile(fs.readFileSync('templates/feed.hbs').toString());
+feedItems.sort((a, b) => { return a.pubDate < b.pubDate});
+const feedTemplate =  Handlebars.compile(fs.readFileSync('templates/feed.hbs').toString());
 fs.writeFileSync('feed.xml', feedTemplate({feedItems: feedItems}));
